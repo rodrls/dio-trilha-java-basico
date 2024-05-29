092b152be2af1e4bc9e38e0dda6cabda7f8b50a7
rodrigo lopes <rodrigolss@yahoo.com>
2024-05-29 20:54:46 -0300

    contaBanco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
+    
+
+    /*public criarConta(int numero, String agencia, String nome){
+        return 
+    }*/
+
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //System.out.println("Hello, World!");
+
+        double saldo = 237.48;
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o numero da agencia");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite o numero da conta");
+        String agencia = scanner.next();
+
+        System.out.println("Digite seu nome");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenomeCliente = scanner.next();
+
+        System.out.println("Olá " +nomeCliente +" " +sobrenomeCliente + "! obrigado por criar uma conta em nosso banco, o numero de sua agência é: "+agencia + " e o numero de sua conta é: "+numero +". Seu saldo de " +saldo +" já está disponível para saque");
+        
+
     }
 }
